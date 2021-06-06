7b9672be947e4ef848b2fc5e231535b39f96943c
Alexei A. Smekalkine <ikle@ikle.ru>
2021-06-06 17:10:40 +0300

    trellis-conf: restyle a bit

--- trellis-conf.h
+++ trellis-conf.h
@@ -16,16 +16,16 @@ struct config_action {
 	int (*on_comment)   (void *o, const char *value);
 	int (*on_sysconfig) (void *o, const char *name, const char *value);
 
-	int (*on_tile)    (void *o, const char *name);
-	int (*on_arc)     (void *o, const char *sink, const char *source);
-	int (*on_word)    (void *o, const char *name, const char *value);
-	int (*on_enum)    (void *o, const char *name, const char *value);
-	int (*on_raw)     (void *o, const char *value);
+	int (*on_tile)      (void *o, const char *name);
+	int (*on_arc)       (void *o, const char *sink, const char *source);
+	int (*on_word)      (void *o, const char *name, const char *value);
+	int (*on_enum)      (void *o, const char *name, const char *value);
+	int (*on_raw)       (void *o, const char *value);
 
 	int (*on_bram)      (void *o, const char *name);
 	int (*on_bram_data) (void *o, unsigned value);
 
-	int (*on_commit) (void *o);
+	int (*on_commit)    (void *o);
 };
 
 struct config {
