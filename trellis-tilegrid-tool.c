fe1315b3be58b961f303afe29acf35a3fed8c4e4
Alexei A. Smekalkine <ikle@ikle.ru>
2021-06-10 01:09:52 +0300

    trellis-tilegrid: compute database paths from environment and arguments

--- trellis-tilegrid-tool.c
+++ trellis-tilegrid-tool.c
@@ -7,9 +7,56 @@
  */
 
 #include <err.h>
+#include <stdlib.h>
 
 #include <cmdb.h>
+#include <dakota/string.h>
 #include <json-c/json.h>
+
+static json_object *trellis_open_grid (const char *family, const char *device)
+{
+	static const char *prefix;
+	static const char *trellis;
+	char *path;
+	json_object *root;
+
+	if (prefix == NULL && (prefix = getenv ("PREFIX")) == NULL)
+		prefix = "/usr";
+
+	if (trellis == NULL && (trellis = getenv ("TRELLIS")) == NULL)
+		trellis = make_string ("%s/share/trellis/database", prefix);
+
+	if (trellis == NULL)
+		return NULL;
+
+	path = make_string ("%s/%s/%s/tilegrid.json", trellis, family, device);
+	if (path == NULL)
+		return NULL;
+
+	root = json_object_from_file (path);
+	free (path);
+	return root;
+}
+
+static struct cmdb *
+dakota_open_grid (const char *family, const char *device, const char *mode)
+{
+	static const char *home;
+	char *path;
+	struct cmdb *db;
+
+	if (home == NULL && (home = getenv ("HOME")) == NULL)
+		return NULL;
+
+	path = make_string ("%s/.cache/dakota/db/%s-%s.cmdb",
+			    home, family, device);
+	if (path == NULL)
+		return NULL;
+
+	db = cmdb_open (path, mode);
+	free (path);
+	return db;
+}
 
 static const char *json_fetch (json_object *root, const char *name)
 {
@@ -40,12 +87,12 @@ int main (int argc, char *argv[])
 	json_object *root;
 
 	if (argc != 3)
-		err (0, "\n\ttrellis-tilegrid <out-cmdb> <in-json>");
+		err (0, "\n\ttrellis-tilegrid <family> <device>");
 
-	if ((db = cmdb_open (argv[1], "rwx")) == NULL)
+	if ((db = dakota_open_grid (argv[1], argv[2], "rwx")) == NULL)
 		errx (1, "cannot open database");
 
-	if ((root = json_object_from_file (argv[2])) == NULL)
+	if ((root = trellis_open_grid (argv[1], argv[2])) == NULL)
 		errx (1, "cannot open tile grid database");
 
 	json_object_object_foreach (root, key, child)
