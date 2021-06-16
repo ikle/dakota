ab55ed9a632416f12594a35ad51772956ef9d264
Alexei A. Smekalkine <ikle@ikle.ru>
2021-06-16 23:27:15 +0300

    model: restyle header a bit

--- include/dakota/model.h
+++ include/dakota/model.h
@@ -17,11 +17,11 @@ void model_free (struct model *o);
 
 const char *model_status (struct model *o);
 
-int model_add_input  (struct model *o, const char *name);
-int model_add_output (struct model *o, const char *name);
-int model_add_wire   (struct model *o, const char *sink, const char *source);
-int model_add_cell   (struct model *o, const char *type, const char *name);
-int model_add_model  (struct model *o, const char *name);
+int model_add_input    (struct model *o, const char *name);
+int model_add_output   (struct model *o, const char *name);
+int model_add_wire     (struct model *o, const char *sink, const char *source);
+int model_add_cell     (struct model *o, const char *type, const char *name);
+int model_add_model    (struct model *o, const char *name);
 
 /*
  * model aoi22
@@ -32,11 +32,11 @@ int model_add_model  (struct model *o, const char *name);
  *	+ cell nor U2 + wire U2.A U0.Y + wire U2.B U1.Y + wire Y U2.Y
  */
 
+int model_add_tuple    (struct model *o, int size, ...);
 int model_add_tuple_va (struct model *o, int size, va_list ap);
-int model_add_tuple  (struct model *o, int size, ...);
-int model_add_param  (struct model *o, const char *name, const char *value);
-int model_add_attr   (struct model *o, const char *name, const char *value);
+int model_add_param    (struct model *o, const char *name, const char *value);
+int model_add_attr     (struct model *o, const char *name, const char *value);
 
-int model_commit     (struct model *o);
+int model_commit       (struct model *o);
 
 #endif  /* DAKOTA_MODEL_H */
