9c0dd2aeb4441de70a8dc6571e943c4ba821af2f
Alexei A. Smekalkine <ikle@ikle.ru>
2021-06-16 19:01:29 +0300

    model-connect: check all model ports at once, simplify

--- model-connect.c
+++ model-connect.c
@@ -116,35 +116,6 @@ static int model_bind_cell (struct model *o, struct cell *cell)
 	return 1;
 }
 
-static int model_check_cell (struct model *o, struct cell *cell)
-{
-	struct model *m;
-	size_t i;
-	char *name;
-	int ok;
-
-	if ((m = model_get_model (o, cell->type)) == NULL)
-		return error (&o->error, "cannot find model %s for cell %s",
-			      cell->type, cell->name);
-
-	for (i = 0; i < m->nports; ++i) {
-		if (m->port[i].type != PORT_DRIVEN)  /* is not input? */
-			continue;
-
-		name = make_string ("%s.%s", cell->name, m->port[i].name);
-		if (name == NULL)
-			return error (&o->error, NULL);
-
-		ok = model_has_sink (o, M_UNKNOWN, name);
-		free (name);
-
-		if (!ok)
-			return 0;
-	}
-
-	return 1;
-}
-
 int model_connect (struct model *o)
 {
 	size_t i;
@@ -158,19 +129,7 @@ int model_connect (struct model *o)
 			return 0;
 
 	for (i = 0; i < o->nports; ++i)
-		if (o->port[i].type == 0 &&  /* is output? */
-		    !model_has_sink (o, i, NULL))
-			return 0;
-
-	for (i = 0; i < o->nwires; ++i) {
-		o->wire[i].from = model_get_sink (o, M_UNKNOWN,
-						  o->wire[i].source);
-		if (o->wire[i].from == M_UNKNOWN)
-			return 0;
-	}
-
-	for (i = 0; i < o->ncells; ++i)
-		if (!model_check_cell (o, o->cell + i))
+		if (!model_has_sink (o, i, o->port[i].name))
 			return 0;
 
 	for (i = 0; i < o->nmodels; ++i)
