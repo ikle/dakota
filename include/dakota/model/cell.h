6f826dce4ff76428c3322c07c3b2db16040ae643
Alexei A. Smekalkine <ikle@ikle.ru>
2022-02-24 04:45:26 +0300

    model-cell: reorder fields, restyle header

--- include/dakota/model/cell.h
+++ include/dakota/model/cell.h
@@ -17,17 +17,11 @@
 #include <dakota/data/tuple.h>
 
 struct cell {
-	char *type;
-	char *name;
+	char *type, *name;
 
-	size_t        nbinds;
-	struct pair  *bind;
-	size_t        ntuples;
+	size_t       nbinds, nparams, nattrs, ntuples;
+	struct pair  *bind,  *param,  *attr;
 	struct tuple *tuple;
-	size_t        nparams;
-	struct pair  *param;
-	size_t        nattrs;
-	struct pair  *attr;
 
 	struct bitmap *map;
 };
@@ -36,12 +30,12 @@ int  cell_init (struct cell *o, const char *type, const char *name);
 void cell_fini (struct cell *o);
 
 int cell_add_bind     (struct cell *o, const char *port, const char *value);
+int cell_add_param    (struct cell *o, const char *name, const char *value);
+int cell_add_attr     (struct cell *o, const char *name, const char *value);
 
 int cell_add_tuple_va (struct cell *o, int size, va_list ap);
 int cell_add_tuple_v  (struct cell *o, int size, const char *argv[]);
 int cell_add_tuple    (struct cell *o, int size, ...);
-int cell_add_param    (struct cell *o, const char *name, const char *value);
-int cell_add_attr     (struct cell *o, const char *name, const char *value);
 
 int cell_load_bitmap  (struct cell *o, const char *path);
 
